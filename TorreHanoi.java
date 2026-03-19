b19b0e6cf1998b3130fd9ff6b502ada9680a743b
GabrielCampos <gabrielfilipecampos1@gmail.com>
2026-03-19 14:18:19 +0000

    Torre

---
+++ TorreHanoi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TorreHanoi {
+        public static void resolverTorreHanoi(int n,char origem,char destino,char auxiliar) {
+        if(n==1){
+            System.out.println("Mover disco 1 de" +origem+ "para" +destino);
+            return;
+        }
+        resolverTorreHanoi(n-1,origem,auxiliar,destino);
+        System.out.println("Mover disco " +n+ "de " +origem+ "para " +destino);
+
+        resolverTorreHanoi(n-1,auxiliar,destino,origem);
+    }
+}
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n;
+        System.out.println("Digite o número de pinos que tem sua torre de hanoi");
+        n = input.nextInt();
+    }
